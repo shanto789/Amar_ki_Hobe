ea5bba66b2159d1c7be79f5c126be58e977d2469
shanto789 <shanto789@gmail.com>
2023-05-14 23:25:04 -0500

    All rights reserved to smarttech and updated may 13, 23

---
+++ src/test/java/batch34/Car.java
@@ -0,0 +1,17 @@
+package batch34;
+
+public class Car {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int cash = 100;
+		double num = 10.00;
+		double dollar = 50.55;
+		double summary = cash+num+dollar;
+		String name= "smarttech";
+		System.out.println(name +  "total cost ="+ summary);
+		
+	}
+
+}
